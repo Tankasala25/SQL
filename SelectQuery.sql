5556c73cf828514394ea22ad44f93885aa769b20
Tankasala25 <tankasala.usedu1@gmail.com>
2025-03-22 21:05:29 -0400

    Updated select query

--- SelectQuery.sql
+++ SelectQuery.sql
@@ -1,1 +1,12 @@
-SELECT * from employees;
+--1.Using the SELECT FROM statement to retrieve data from a single column example
+
+select lastName from employees;
+
+
+--2) Using the SELECT FROM statement to query data from multiple columns example
+
+select lastName, firstName, jobTitle from dbo.employees;
+
+/*Summary
+Use the SELECT FROM statement to select data from a table.
+Use the SELECT * to select data from all columns of a table.*/
